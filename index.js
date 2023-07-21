d30b8bef9b313aa740a25ab1474187c0385faea1
Ajin Abraham <105640840+ajin009@users.noreply.github.com>
2023-07-21 09:36:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'this is my first node js application created on july 20 08:5AM\n'
+  const msg = 'this is my first node js application created on july 21 09:36\n'
   res.end(msg);
 });
 
